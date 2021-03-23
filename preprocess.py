7b70a536126c0e4c55f933d3d271ef0117e3d35f
soerenetler <soeren.etler@gmail.com>
2021-03-23 19:19:48 +0100

    en_core_web_trf

--- preprocess.py
+++ preprocess.py
@@ -47,7 +47,8 @@ if __name__ == "__main__":
     df_askable_paragraph_test["askable_tokens"] = [ast.literal_eval(t) for t in df_askable_paragraph_test["askable_tokens"]]
     df_askable_paragraph_test["sentence_tokens"] = [ast.literal_eval(t) for t in df_askable_paragraph_test["sentence_tokens"]]
 
-    nlp = spacy.load('en_core_web_sm')
+    nlp = spacy.load('en_core_web_trf')
+    spacy.require_gpu()
     nlp.tokenizer = nlp.tokenizer.tokens_from_list
 
     feature_transformer = FeatureTransformer(nlp)
