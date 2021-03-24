0b8c2e13646e79bc6896f74eb32fc34f3c445d59
soerenetler <soeren.etler@gmail.com>
2021-03-24 14:09:44 +0100

    print device

--- FeatureTransformer.py
+++ FeatureTransformer.py
@@ -156,6 +156,10 @@ class FeatureTransformer(TransformerMixin):
     def text2features(self, sent):
         token = [token.text for token in sent]
         allennlp.nn.util.move_to_device(token, 0)
+
+        print(allennlp.nn.util.get_device_of(token))
+        print(allennlp.nn.util.get_device_of(self.predictor))
+
         srl_pred = self.predictor.predict_tokenized(token)
         if not srl_pred["verbs"]:
             srl_tags = ["O"]*len(sent)
