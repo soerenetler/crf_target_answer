5a1432c4e2eb29ec85a5c718befa0ffcacd6a498
soerenetler <soeren.etler@gmail.com>
2021-03-28 14:52:27 +0200

    typo

--- parameter_tuning.py
+++ parameter_tuning.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
 
     train_feature = train_data["x"] + dev_data["x"]
     
-    y_train = data["y"] + dev_data["y"]
+    y_train = train_data["y"] + dev_data["y"]
 
     crf = Custom_CRF(algorithm='lbfgs',
                     max_iterations=100,
