3c670f224918e562e825d34b7ae3d1b8982bcd18
soerenetler <soeren.etler@gmail.com>
2021-04-11 23:04:14 +0200

    typo

--- FeatureTransformer.py
+++ FeatureTransformer.py
@@ -140,6 +140,6 @@ class FeatureTransformer(TransformerMixin):
             srl_tags = []
             srl_verb = []
         else:
-            srl_tags = [element{"tags"] for element in srl_pred["verbs"]]
-            srl_verb = [element{"verb"] for element in srl_pred["verbs"]]
+            srl_tags = [element["tags"] for element in srl_pred["verbs"]]
+            srl_verb = [element["verb"] for element in srl_pred["verbs"]]
         return [self.word2features(sent, srl_tags, srl_verb, i) for i in range(len(sent))]
